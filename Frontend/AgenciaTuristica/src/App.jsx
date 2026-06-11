94dd1e33bcd064d160cc774c2d5a7492b61c2294
Tania Garcia <paolag.230805@gmail.com>
2026-06-11 09:35:29 -0600

    actualizacion de cambios

---
+++ Frontend/AgenciaTuristica/src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from './components/common/Navbar.jsx'
+import Footer from './components/common/Footer.jsx'
+
+function App() {
+  return (<div className="App">
+      <Navbar />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
